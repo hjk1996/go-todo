999c2abd8c29a13377d4bd68542cf92a35e9a928
hjk1996 <dunhill741@naver.com>
2024-01-06 16:55:00 +0900

    complete go todo app

--- main.go
+++ main.go
@@ -1,29 +1,81 @@
 package main
 
 import (
-	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type Todo struct {
+	ID        int       `gorm:"column:id"`
+	Task      string    `gorm:"column:task"`
+	Done      bool      `gorm:"column:done"`
+	CreatedAt time.Time `gorm:"column:created_at"`
+}
 
+func getTodos(db *gorm.DB) []Todo {
+	var todos []Todo
+	db.Find(&todos)
+	fmt.Println(todos)
+	return todos
+}
 
-func main () {
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	// initialize router
 	r := gin.Default()
+	// load HTML templates
+	r.LoadHTMLGlob("templates/*")
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	// connect to db
-	db, err := sql.Open("mysql", "todoadmin:test1234@tcp(127.0.0.1:3306)/gotodo")
 	if err != nil {
 		log.Fatal(err)
-	}	
-	defer db.Close()
-	// start 
+	}
 
-	r.Run()
+	r.GET("/", func(c *gin.Context) {
+		todos := getTodos(db)
+		fmt.Println(todos)
+		c.HTML(http.StatusOK, "index.html", todos)
+	})
 
+	r.POST("/add-task", func(c *gin.Context) {
+		// 제출된 form에서 task란 이름의 값 찾음
+		task := c.PostForm("task")
+		newTodo := Todo{
+			Task: task,
+			Done: false,
+		}
+		res := db.Create(&newTodo)
 
-	
+		if res.Error != nil {
+			fmt.Println(res.Error)
+			log.Fatal(res.Error)
+		}
+		fmt.Println(res)
+		// 루트로 리다이렉트
+		c.Redirect(http.StatusFound, "/")
+	})
+
+	r.POST("/delete-task", func(c *gin.Context) {
+		taskId := c.PostForm("taskId")
+		// id가 taskId와 동일한 Todo 삭제
+		db.Delete(&Todo{}, taskId)
+		c.Redirect(http.StatusFound, "/")
+	})
+
+	r.Run("localhost:8080")
+
 }
