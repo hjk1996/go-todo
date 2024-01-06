8c8b4f2cf9e5b03dbb4b5591e55d3b4e869e1886
hjk1996 <dunhill741@naver.com>
2024-01-06 14:34:34 +0900

    add todo schema

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
 
 
 func main () {
+
+	// initialize router
+	r := gin.Default()
+	// connect to db
+	db, err := sql.Open("mysql", "todoadmin:test1234@tcp(127.0.0.1:3306)/gotodo")
+	if err != nil {
+		log.Fatal(err)
+	}	
+	defer db.Close()
+	// start 
+
+	r.Run()
+
+
 	
 }
